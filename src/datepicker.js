997cf464c1e7e40acb87eeb0be4a8600dbf3705b
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2013-09-18 08:24:27 -0300

    kick-off

---
+++ src/datepicker.js
@@ -0,0 +1,37 @@
+angular.module('components', []).
+  directive('dateInput', function() {
+    
+    return {
+      restrict: 'E',
+      scope: {
+        ngModel: "="
+      },
+  template: "<div class='input-append'><input date-picker ng-model='ngModel' class='input-small' type='text'/><span class='add-on' style='cursor: pointer'><i class='icon-calendar'></i></span></div>",
+      link: function (scope, element, attrs, ctrl) {
+        $(element).find(".add-on").click(function() {
+          $(element).find("input[type='text']").focus().select(); 
+        }); 
+      }
+    }
+  
+  }).
+  directive('datePicker', function() {
+
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModelCtrl)
+        {
+          $(element).datepicker({
+            dateFormat: "yy-mm-dd",
+            onSelect: function(date) {
+              ngModelCtrl.$setViewValue(date);
+              scope.$apply();
+            }
+            
+          }); 
+        }
+      }
+  
+  });
+  
